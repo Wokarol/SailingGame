bdde5d52584222cca7ed7d7a6abb890cb08b99cf
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2020-01-17 21:25:45 +0100

    Added basic movement

--- SailingGame/Assets/Game/Gameplay Code/Runtime/ShipMotor/ShipMotor.cs
+++ SailingGame/Assets/Game/Gameplay Code/Runtime/ShipMotor/ShipMotor.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 namespace Wokarol.Ship
 {
+	[RequireComponent(typeof(Rigidbody2D))]
 	public class ShipMotor : MonoBehaviour
 	{
+		[SerializeField] private float MaxVelocity = 1f;
+
 		public IMotorInput Input { get; set; }
 
+		private Rigidbody2D body;
 
+		private void Awake()
+		{
+			body = GetComponent<Rigidbody2D>();
+		}
+
+		private void Update()
+		{
+			Vector2 direction = Input.Direction;
+			float velocity = Input.SailPower * MaxVelocity;
+
+
+			body.rotation = Vector2.SignedAngle(Vector2.down, direction);
+			body.velocity = direction * velocity;
+		}
 	}
 }
