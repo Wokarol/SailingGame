adfdc5acb5d58b5b7aa2b2b4c40f59651a239e71
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2020-01-25 16:13:23 +0100

    Modified order in Ship Motor, added weapon gizmo, added suppression,

--- SailingGame/Assets/Game/Ships/ShipMotor/ShipMotor.cs
+++ SailingGame/Assets/Game/Ships/ShipMotor/ShipMotor.cs
@@ -4,52 +4,56 @@ using UnityEngine;
 
 namespace Wokarol.Ship
 {
-	[RequireComponent(typeof(Rigidbody2D))]
-	public class ShipMotor : MonoBehaviour
-	{
-		[SerializeField, Tooltip("m/s")] private float MaxVelocity = 1f;
-		[Space]
-		[SerializeField, Tooltip("m/s^2")] private float AccelerationSpeed = 0.5f;
-		[SerializeField, Tooltip("m/s^2")] private float DecelerationSpeed = 0.5f;
-		[Space]
-		[SerializeField, Tooltip("degrees/s")] private float RotationSpeed = 90;
-		[Tooltip("x{0, max velocity}  y{0, rotation speed}")]
-		[SerializeField] private AnimationCurve velocityToRotationSpeed = AnimationCurve.Linear(0, 0.25f, 1, 1);
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class ShipMotor : MonoBehaviour
+    {
+        [SerializeField, Tooltip("m/s")] private float MaxVelocity = 1f;
+        [Space]
+        [SerializeField, Tooltip("m/s^2")] private float AccelerationSpeed = 0.5f;
+        [SerializeField, Tooltip("m/s^2")] private float DecelerationSpeed = 0.5f;
+        [Space]
+        [SerializeField, Tooltip("degrees/s")] private float RotationSpeed = 90;
+        [Tooltip("x{0, max velocity}  y{0, rotation speed}")]
+        [SerializeField] private AnimationCurve velocityToRotationSpeed = AnimationCurve.Linear(0, 0.25f, 1, 1);
 
-		public IMotorInput Input { get; set; }
+        public IMotorInput Input { get; set; }
 
-		private Rigidbody2D body;
+        private Rigidbody2D body;
 
-		private float velocity = 0;
-		private Quaternion rotation = Quaternion.identity;
+        private float velocity = 0;
+        private Quaternion rotation = Quaternion.identity;
 
-		private void Awake()
-		{
-			body = GetComponent<Rigidbody2D>();
-			if (!body) Debug.LogError($"No Rigidbody2D found on {nameof(ShipMotor)}!", this);
-		}
+        private void Awake()
+        {
+            body = GetComponent<Rigidbody2D>();
+            if (!body) Debug.LogError($"No Rigidbody2D found on {nameof(ShipMotor)}!", this);
+        }
 
-		private void Update()
-		{
-			// Calculates new direction
-			Quaternion targetRotation = Quaternion.FromToRotation(Vector2.down, Input.Direction);
-			float currentRotationSpeed = RotationSpeed * velocityToRotationSpeed.Evaluate(velocity / MaxVelocity);
-			rotation = Quaternion.RotateTowards(rotation, targetRotation, currentRotationSpeed * Time.deltaTime);
-			
-			// Calculates new target velocity
-			float targetVelocity = Input.SailPower * MaxVelocity;
-			float velocityChangeSpeed = targetVelocity > velocity ? AccelerationSpeed : DecelerationSpeed;
+        private void FixedUpdate()
+        {
 
-			// Calculates new velocity
-			velocity = Mathf.MoveTowards(velocity, targetVelocity, velocityChangeSpeed * Time.deltaTime);
+            // Calculates new target velocity
+            float targetVelocity = Input.SailPower * MaxVelocity;
+            float velocityChangeSpeed = targetVelocity > velocity ? AccelerationSpeed : DecelerationSpeed;
 
-			// Uses calculated data to move ship via rigidbody
-			if (body) {
-				transform.rotation = rotation;
-				body.velocity = -transform.up * velocity; 
-			} else {
-				Debug.LogError($"Rigidbody is null on {nameof(ShipMotor)}", this);
-			}
-		}
-	}
+            // Calculates new velocity
+            velocity = Mathf.MoveTowards(velocity, targetVelocity, velocityChangeSpeed * Time.deltaTime);
+
+            // Calculates new direction
+            Quaternion targetRotation = Quaternion.FromToRotation(Vector2.up, Input.Direction);
+            float currentRotationSpeed = RotationSpeed * velocityToRotationSpeed.Evaluate(velocity / MaxVelocity);
+            rotation = Quaternion.RotateTowards(rotation, targetRotation, currentRotationSpeed * Time.deltaTime);
+
+            // Uses calculated data to move ship via rigidbody
+            if (body)
+            {
+                transform.rotation = rotation;
+                body.velocity = transform.up * velocity;
+            }
+            else
+            {
+                Debug.LogError($"Rigidbody is null on {nameof(ShipMotor)}", this);
+            }
+        }
+    }
 }
---
+++ SailingGame/Assets/Game/Ships/ShipWeapons/Code/ShipCanons.cs
@@ -0,0 +1,67 @@
+﻿using NaughtyAttributes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCanons : MonoBehaviour
+{
+    [SerializeField, Tooltip("Canon distance, in meters")] private float maxDistance = 5;
+    [Space]
+    [Tooltip("Spread of canons in degrees, half the angle"), Range(0, 90)]
+    [SerializeField] private float angleSpread = 15;
+    [Tooltip("Spread of conons in meters"), Range(0, 1)]
+    [SerializeField] private float positionSpread = 0.2f;
+    [SerializeField] private int canonCount = 5;
+
+    private void OnValidate()
+    {
+        angleSpread = Mathf.Max(0, angleSpread);
+        positionSpread = Mathf.Max(0, positionSpread);
+        canonCount = Mathf.Max(2, canonCount);
+    }
+
+    private Ray GetShootRay(float t)
+    {
+        float angle = Mathf.Lerp(-angleSpread, angleSpread, t);
+        float offset = Mathf.Lerp(-positionSpread, positionSpread, t);
+
+        Vector3 dir = GetDirectionForAngle(angle);
+        Vector3 pos = transform.position - transform.right * offset;
+
+        return new Ray(pos, dir);
+    }
+
+    private Vector3 GetDirectionForAngle(float angle)
+    {
+        return Quaternion.AngleAxis(angle, transform.forward) * transform.up;
+    }
+
+    private void OnDrawGizmos()
+    {     
+        float interval = 1f / (canonCount - 1);
+
+        Ray lastRay = new Ray();
+
+        for (int i = 0; i < canonCount; i++)
+        {
+            float t = interval * i;
+            Ray ray = GetShootRay(t);
+
+            Gizmos.color = i == 0 || i == canonCount - 1
+                ? Color.red
+                : Color.red * new Color(1, 1, 1, 0.3f);
+
+            Gizmos.DrawRay(ray.origin, ray.direction * maxDistance);
+
+            if(i != 0)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(ray.GetPoint(maxDistance), lastRay.GetPoint(maxDistance));
+                Gizmos.DrawLine(ray.origin, lastRay.origin);
+            }
+
+            lastRay = ray;
+        }
+    }
+}
